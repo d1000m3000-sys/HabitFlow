0b087649dc2bc9e5274b573c4e54483ad5d62079
d1000m3000-sys <d1000m3000@gmail.com>
2026-04-16 17:25:04 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+let habits = JSON.parse(localStorage.getItem("habits")) || [];
+
+function save() {
+    localStorage.setItem("habits", JSON.stringify(habits));
+}
+
+function addHabit() {
+    let input = document.getElementById("habitInput");
+    if (input.value === "") return;
+
+    habits.push({
+        name: input.value,
+        done: false
+    });
+
+    input.value = "";
+    save();
+    render();
+}
+
+function toggleHabit(index) {
+    habits[index].done = !habits[index].done;
+    save();
+    render();
+}
+
+function deleteHabit(index) {
+    habits.splice(index, 1);
+    save();
+    render();
+}
+
+function render() {
+    let list = document.getElementById("habitList");
+    list.innerHTML = "";
+
+    habits.forEach((h, i) => {
+        list.innerHTML += `
+        <div class="habit">
+            <span class="${h.done ? 'done' : ''}" onclick="toggleHabit(${i})">
+                ${h.name}
+            </span>
+            <button onclick="deleteHabit(${i})">❌</button>
+        </div>`;
+    });
+}
+
+render();
